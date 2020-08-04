32b68f1d8b634cc224859629ccdb4d21fb09a157
shiva <shiva>
2020-08-04 05:10:08 -0400

    first commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,  get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request , 'index.html' , {'posts': posts})
+
+def post(request, slug):
+    print(slug)
+    return render(None, 'post.html', {
+        'post':get_object_or_404(Post , slug=slug)
+    })
+
+def about(request):
+    return render(request , 'about.html')
